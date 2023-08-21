ef18a13134a4d616a9793acb90a61e90e6394b9d
cross03kohei <140483428+cross03kohei@users.noreply.github.com>
2023-08-21 14:09:09 +0900

    first commit

---
+++ src/main/java/jp/cross/client/clientmanage/ClientManageApplication.java
@@ -0,0 +1,13 @@
+package jp.cross.client.clientmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/cross/client/clientmanage/ClientManageApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.cross.client.clientmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
