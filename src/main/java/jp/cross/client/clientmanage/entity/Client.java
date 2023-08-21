25b9461ac3a5d4c50fa57c8b22e0570b31ebb678
cross03kohei <140483428+cross03kohei@users.noreply.github.com>
2023-08-21 14:28:39 +0900

    lombookを使用するためにpom.xmlに依存関係を記述

--- src/main/java/jp/cross/client/clientmanage/entity/Client.java
+++ src/main/java/jp/cross/client/clientmanage/entity/Client.java
@@ -1,4 +1,13 @@
 package jp.cross.client.clientmanage.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "client") //テーブル名の指定
+
 public class Client {
+
 }
