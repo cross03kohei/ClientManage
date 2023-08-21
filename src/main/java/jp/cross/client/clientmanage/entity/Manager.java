92e9a97680db43f33d30fe67ec9a66483e297294
cross03kohei <140483428+cross03kohei@users.noreply.github.com>
2023-08-21 15:22:26 +0900

    entityクラスのmanagerの実装

--- src/main/java/jp/cross/client/clientmanage/entity/Manager.java
+++ src/main/java/jp/cross/client/clientmanage/entity/Manager.java
@@ -1,4 +1,34 @@
 package jp.cross.client.clientmanage.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "manager")
+@Getter
+@Setter
 public class Manager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //主キーが自動生成される
+    private Integer id;
+
+    /**
+     * 名前
+     */
+    @Column(name="name", nullable = false, length = 30)
+    private String name;
+
+    /**
+     * 所属
+     */
+    @Column(name="affiliation", nullable = false, length = 30)
+    private String affiliation;
+
+    @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL)
+    private List<Proceeds> proceeds;
+
 }
