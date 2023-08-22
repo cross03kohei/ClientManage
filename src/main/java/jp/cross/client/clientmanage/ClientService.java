301f25a4b940bcd2f2191713fdee5d5d88432ced
cross03kohei <140483428+cross03kohei@users.noreply.github.com>
2023-08-22 12:07:09 +0900

    コントローラーに昨日の追加

--- src/main/java/jp/cross/client/clientmanage/ClientService.java
+++ src/main/java/jp/cross/client/clientmanage/ClientService.java
@@ -1,4 +1,24 @@
 package jp.cross.client.clientmanage;
 
+import jp.cross.client.clientmanage.entity.Proceeds;
+import jp.cross.client.clientmanage.repository.ClientRepository;
+import jp.cross.client.clientmanage.repository.ManagerRepository;
+import jp.cross.client.clientmanage.repository.ProceedsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClientService {
+    @Autowired
+    ClientRepository clientRepository;
+
+    @Autowired
+    ManagerRepository managerRepository;
+
+    @Autowired
+    ProceedsRepository proceedsRepository;
+
+
 }
--- src/main/java/jp/cross/client/clientmanage/HomeController.java
+++ src/main/java/jp/cross/client/clientmanage/HomeController.java
@@ -1,4 +1,30 @@
 package jp.cross.client.clientmanage;
 
+import jp.cross.client.clientmanage.entity.Proceeds;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class HomeController {
+
+    @Autowired
+    ClientService clientService;
+
+    @GetMapping("/index")
+    String clientList(Model model) {
+
+
+        return "index";
+    }
+
+    @GetMapping("/search")
+    String searchList(Model model){
+
+        return "index";
+    }
+
 }
